56d7d79e5f293c1cbcd7a805c7e57925d1c5fda0
Branning <bmk2110@gmail.com>
2023-05-10 15:30:44 -0400

    Restructured Css and began the resume page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,15 +4,17 @@ import Footer from './components/Footer';
 import About from './components/pages/About';
 import Portfolio from './components/pages/Portfolio';
 import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
 
 function App() {
   return (
   <div className="App">
     <Header />
-    <Contact />
+    {/* <Contact /> */}
     {/* <Portfolio /> */}
     {/* <About /> */}
     {/* <Section /> */}
+    <Resume />
     <Footer />
   </div>
   );
---
+++ my-portfolio/src/components/pages/Resume.js
@@ -0,0 +1,11 @@
+import '../../styles/Resume.css';
+
+function Resume() {
+  return (
+    <div className='resumeContainer'>
+      
+    </div>
+  )
+}
+
+export default Resume;
