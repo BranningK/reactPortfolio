01ead492c696bc32070ec96cbcfacb8d238177fc
Branning <bmk2110@gmail.com>
2023-05-11 15:37:50 -0400

    finished Resume page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
   <div className="App">
     <Header />
-    {/* <Contact /> */}
-    {/* <Portfolio /> */}
-    {/* <About /> */}
-    {/* <Section /> */}
-    <Resume />
+      {/* <Contact /> */}
+      {/* <Portfolio /> */}
+      <About />
+      {/* <Section /> */}
+      {/* <Resume /> */}
     <Footer />
   </div>
   );
